68e88e3936b236750f46e711b0eef9802d4435ad
Joseph Simone <jesimone57@yahoo.com>
2017-12-03 19:10:30 -0500

    don't allow negative numbers, 0 and 1 to be prime

--- isprime.scala
+++ isprime.scala
@@ -1,14 +1,15 @@
 // Example usage:  
 //
-// val primes1To100 = for (i <- 2 to 100 if isPrime(i) == true) yield(i)
-// primes1To100  // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
-// primes1To100.length  // Int = 25
+// val primes0To100 = for (i <- 0 to 100 if isPrime(i) == true) yield(i)
+// primes0To100  // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+// primes0To100.length  // Int = 25
 //
+// Note: negative numbers are rejected
 
 def isPrime(n: Int): Boolean = {
   import scala.math
   val oddDivisors = for (i <- 3 to n if i <= Math.sqrt(n)) yield(i)
   val divisors = Vector(2) ++ oddDivisors
-  val divisableBy = for (d <- divisors) yield n>d && n%d == 0
-  ! divisableBy.reduceLeft((a:Boolean, b:Boolean) => a || b)
+  val divisableBy = for (d <- divisors if n>1) yield n>d && n%d == 0
+  if (divisableBy.isEmpty) false else ! divisableBy.reduceLeft((a:Boolean, b:Boolean) => a || b)
 }
