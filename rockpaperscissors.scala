1dbe13d027cbc121a676e90bba8f5f201d9d93a5
Joseph Simone <jesimone57@yahoo.com>
2017-12-04 08:41:53 -0500

    Update rockpaperscissors.scala

--- rockpaperscissors.scala
+++ rockpaperscissors.scala
@@ -9,10 +9,10 @@ val wins = Set(("rock", "scissors"), ("paper", "rock"), ("scissors", "paper"))
 def getPlayerInput(player: Int) = {
   var choice: String = ""
   while (! choices.contains(choice)) {
-    println("Choose rock, paper or scissors?")
+    println("Player %d choose rock, paper or scissors?".format(player))
     choice = scala.io.StdIn.readLine().trim.toLowerCase
-    println("Player %d chose %s".format(player, choice))
   }
+  println("Player %d chose %s \n".format(player, choice))
   choice
 }
 
@@ -24,7 +24,7 @@ if (wins.contains((player1, player2))) {
 } else if (wins.contains((player2, player1))) {
   println("Player 2 wins because %s beats %s".format(player2, player1))
 } else {
-  println("It's a tie.  You'll have to player again because both players choice %s".format(player1))
+  println("It's a tie.  You'll have to play again because both players choice %s".format(player1))
 }
 
 }
