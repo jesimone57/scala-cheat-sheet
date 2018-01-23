641c7d18685f377db449385fae5fe60c1da573e5
Joseph Simone <jesimone57@yahoo.com>
2018-01-23 16:15:46 -0500

    Update square-root-by-iteration.scala

--- square-root-by-iteration.scala
+++ square-root-by-iteration.scala
@@ -1,3 +1,7 @@
+// Calculate the square root of a number using Newton's method
+//   1. start with an initial guess of 1
+//   2. improve the guess until we converge on the answer
+
 def abs(x: Double): Double = if (x<0) -x else x
 
 def isGoodEnough(guess: Double, x: Double): Boolean = if (abs(guess * guess - x) < 0.0000001) true else false
