0fcf0f862c21ed44dc82e7cac5b147ac625517a2
Joseph Simone <jesimone57@yahoo.com>
2018-01-23 14:05:02 -0500

    Update higher-order-functions.scala

--- higher-order-functions.scala
+++ higher-order-functions.scala
@@ -1,6 +1,10 @@
 
 // This example code take from Martin Odersky's Scala course on Coursera Lecture 2.1 - Summing with Higher Order Functions
 
+//-------------------------------------------------------------------------------
+// Example 1 - no functions (note: a function takes a type and returns a type)
+//-------------------------------------------------------------------------------
+
 // First define some simple functions we can use
 def cube(x: Int): Int = x * x * x
 def factorial(x: Int): Int = if (x == 0) 1 else x * factorial(x-1)
@@ -8,7 +12,7 @@ def id(x: Int): Int = x
 
 // The sum of all integers from a to b
 def sumInts(a: Int, b: Int): Int =  
-  if (a > b) 0 else a + sumInts(a + 1, b)
+  if (a > b) 0 else id(a) + sumInts(a + 1, b)
 
 // The sum of all cubes from a to b
 def sumCubes(a: Int, b: Int): Int =  
@@ -17,8 +21,16 @@ def sumCubes(a: Int, b: Int): Int =
 // The sum of all factorials from a to b
 def sumFactorials(a: Int, b: Int): Int =  
   if (a > b) 0 else factorial(a) + sumFactorials(a + 1, b)
-  
-  
+ 
+sumInts(1, 10)          // returns Int = 55
+sumCubes(1, 3)          // returns Int = 36
+sumFactorials(1, 4)     // returns Int = 33
+
+
+//-------------------------------------------------------------------------------
+// Example 2 - pass a summation function as argument to a new function
+//-------------------------------------------------------------------------------
+
 // Can we factor out the common code into 1 single method?  Sure.
 def sum(f: Int => Int, a: Int, b: Int): Int = 
   if (a > b) 0 else f(a) + sum(f, a + 1, b)
@@ -28,20 +40,23 @@ def sumInts2(a: Int, b: Int):  Int = sum(id, a, b)
 def sumCubes2(a: Int, b: Int): Int = sum(cube, a, b)
 def sumFactorials2(a: Int, b: Int): Int = sum(factorial, a, b)
 
-sumInts(1,4)        // returns Int = 10
-sumInts2(1,4)       // same
+sumInts2(1, 10)           // returns Int = 55
+sumCubes2(1, 3)           // returns Int = 36
+sumFactorials2(1, 4)      // returns Int = 33
 
-sumCubes(1,3)       // returns Int = 36
-sumCubes2(1,3)      // same
 
-sumFactorials(1,4)  // returns Int = 33
-sumFactorials2(1,4) // same
+//-------------------------------------------------------------------------------
+// Example 3 - summation with anonymous functions
+//-------------------------------------------------------------------------------
 
-// summation with anonymous functions
 def sumInts3(a: Int, b: Int):  Int = sum(x => x, a, b)
 def sumCubes3(a: Int, b: Int): Int = sum(x => x * x * x, a, b)
 
-// redefine sum to take a function and return a function
+
+//-------------------------------------------------------------------------------
+// Example 4 - redefine sum to take a function and return a function
+//-------------------------------------------------------------------------------
+
 def sum(f: Int => Int): (Int, Int) => Int = {
   def sumF(a: Int, b: Int): Int = if (a > b) 0 else f(a) + sumF(a + 1, b)
   sumF
@@ -59,6 +74,7 @@ sum(x => x * x * x)(1, 3)           // returnx Int 36
 sumCubes4(1,3)                      // same
 
 sum(factorial)(1,4)                 // returns Int 33
+
 sumFactorials4(1,4)                 // same
 
 
