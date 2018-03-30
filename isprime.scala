ee8e2a1c812611260a589312132fe49945be5e3b
Joseph Simone <jesimone57@yahoo.com>
2018-03-30 12:19:35 -0400

    Update isprime.scala

--- isprime.scala
+++ isprime.scala
@@ -4,6 +4,9 @@
 // primes0To100  // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
 // primes0To100.length  // Int = 25
 //
+// or more concisely:
+// (0 to 100).filter(isPrime(_))  // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+//
 // Note: negative numbers are rejected
 
 def isPrime(n: Int): Boolean = {
